3b3a54d09d5c357352787296339c0ea632f09d9d
shaga <shaga@pasonatquila.com>
2015-12-31 08:28:47 +0900

    prepare for public release

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -6,20 +6,10 @@
 var gulp = require('gulp');
 var babel = require('gulp-babel');
 
-var sfTest = require('./run-sf-test');
-
-
 gulp.task('babel', function(){
     return gulp.src('src/**/*.js')
         .pipe(babel())
         .pipe(gulp.dest('./'));
 });
-
-gulp.task('sf-test', function(){
-    return gulp.src('./src/package.xml')
-        .pipe(sfTest())
-        .pipe(gulp.dest('./result/'));
-});
-
 
 gulp.task('default', ['babel']);
